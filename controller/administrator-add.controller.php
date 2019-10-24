c5b6fbed021ff2b2a811b5cf2fb9668e1bf51b4b
Marius Mandal <mariusmandal@gmail.com>
2019-10-24 21:41:37 +0200

    this is gonna take some time

--- controller/administrator-add.controller.php
+++ controller/administrator-add.controller.php
@@ -5,6 +5,11 @@ use UKMNorge\Wordpress\User;
 
 require_once('UKM/Autoloader.php');
 
+if( !isset( $_GET['debug'] ) ) {
+    throw new Exception(
+        'Beklager, vi jobber med å rette en feil i funksjonen. Antatt ferdig innen kl 22:00'
+    );
+}
 switch( $_GET['type'] ) {
     case 'fylke':
         UKMNettverket::addViewData('fylke', Fylker::getById($_GET['omrade']));
