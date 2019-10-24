111780f6bff22d36ff9eb76a4fe92b9fcf379b26
Marius Mandal <mariusmandal@gmail.com>
2019-10-24 20:56:26 +0200

    Bruk refaktorert Mailchimp

--- controller/administrator-manage.controller.php
+++ controller/administrator-manage.controller.php
@@ -2,6 +2,7 @@
 
 use UKMNorge\API\Mailchimp\Liste\Arrangor;
 use UKMNorge\API\Mailchimp\Mailchimp;
+use UKMNorge\API\Mailchimp\Subscriber;
 use UKMNorge\Wordpress\User;
 use UKMNorge\Wordpress\WriteUser;
 use UKMNorge\Nettverk\Administrator;
@@ -15,18 +16,18 @@ require_once('UKM/Autoloader.php');
 /**
  *  FJERN ADMINISTRATOR
  */
-if( isset( $_GET['removeAdmin'] ) ) {
-    $omrade = Omrade::getByType( 
-        $_GET['type'], 
-        (Int) $_GET['omrade'], 
-        (Int) get_option('season')
+if (isset($_GET['removeAdmin'])) {
+    $omrade = Omrade::getByType(
+        $_GET['type'],
+        (int) $_GET['omrade'],
+        (int) get_option('season')
     );
-    $admin = $omrade->getAdministratorer()->get( (Int) $_GET['removeAdmin'] );
-    $res = WriteOmrade::fjernAdmin( $omrade, $admin );
-    if( $res ) {
+    $admin = $omrade->getAdministratorer()->get((int) $_GET['removeAdmin']);
+    $res = WriteOmrade::fjernAdmin($omrade, $admin);
+    if ($res) {
         UKMnettverket::getFlash()->add(
             'success',
-            $admin->getUser()->getNavn() .' er fjernet som administrator for '. $omrade->getNavn()
+            $admin->getUser()->getNavn() . ' er fjernet som administrator for ' . $omrade->getNavn()
         );
     }
 }
@@ -59,72 +60,73 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $user = WriteUser::save($user);
 
         // Deaktiverte brukere reaktiveres og får nytt passord
-        if( !User::erAktiv( $user->getId() ) ) {
+        if (!User::erAktiv($user->getId())) {
             WriteUser::aktiver($user);
             $passord = WriteUser::genererPassord();
-            WriteUser::setPassord( $user, $passord );
-            WriteUser::sendNyttPassord( $user->getNavn(), $user->getEpost(), $passord );
+            WriteUser::setPassord($user, $passord);
+            WriteUser::sendNyttPassord($user->getNavn(), $user->getEpost(), $passord);
         }
-        
-        $omrade = Omrade::getByType( 
-            $_POST['omrade_type'], 
-            (Int) $_POST['omrade_id'], 
-            (Int) get_option('season')
+
+        $omrade = Omrade::getByType(
+            $_POST['omrade_type'],
+            (int) $_POST['omrade_id'],
+            (int) get_option('season')
         );
-        $administrator = new Administrator( $user->getId() );
-        WriteOmrade::leggTilAdmin( $omrade, $administrator );
-        
+        $administrator = new Administrator($user->getId());
+        WriteOmrade::leggTilAdmin($omrade, $administrator);
+
         // Abonner på blog 1 - alle må det.
         Blog::leggTilBruker(1, $user->getId(), 'subscriber');
-        
+
         // Send velkommen-epost
-        if( !$created ) {
-            WriteOmrade::sendVelkommenTilNyttOmrade( $user->getName(), $user->getEmail(), $omrade );
+        if (!$created) {
+            WriteOmrade::sendVelkommenTilNyttOmrade($user->getName(), $user->getEmail(), $omrade);
         }
-        
-        // Admin må abonnere på nyhetsbrevet vårt
-        $arrangorliste = Arrangor::getList();
-        $arrangorliste->addSubscriber(
-            [
-                'email_address' => $user->getEmail(),
-                'first_name' => $user->getFirstName(),
-                'last_name' => $user->getLastName(),
-            ]
+
+        // Opprett Subscriber-instance for administrator
+        $subscriber = Subscriber::createFromDetails(
+            $user->getEmail(),
+            $user->getFirstName(),
+            $user->getLastName()
         );
-        $mailchimp = new Mailchimp();
-        $mailchimp->saveListChanges( $arrangorliste );
 
-        // Tagg vår nye administrator
-        $tags = [
-            'UKMadmin',
-            'UKM'.get_site_option('season'),
-            $omrade->getType() == 'kommune' ? 'lokalkontakt' : 'fylkeskontakt',
-            $omrade->getId(),
-            $omrade->getNavn(),
-        ];
-
-        foreach( $tags as $index => $tag ) {
-            $tags[ $index ] = Mailchimp::sanitizeTag( $tag );
-        }
         try {
-            Arrangor::tag( $user->getEmail(), $tags );
-        } catch( Exception $e ) {
-            error_log('MAILCHIMP ERROR');
-            error_log('MSG: '. $e->getMessage());
-            error_log('CODE: '. $e->getCode());
+            // Abonner på Arrangør-lista
+            Arrangor::subscribe($subscriber);
+            // Legg til tags
+            $tags = Arrangor::addTags(
+                $subscriber,
+                [
+                    'UKMadmin',
+                    'UKM' . get_site_option('season'),
+                    ($omrade->getType() == 'kommune' ? 'lokalkontakt' : 'fylkeskontakt'),
+                    $omrade->getId(),
+                    $omrade->getNavn(),
+                ]
+            );
+            // Håndter tag-error
+            if ($tags->hasError()) {
+                $message = '';
+                foreach( $tags->getError() as $error ) {
+                    $message .= $error->getMessage() .', ';
+                }
+                throw new Exception( rtrim( $message, ', ') );
+            }
+        } catch (Exception $e) {
+            // Håndter 
             UKMnettverket::getFlash()->add(
                 'info',
-                'En feil med nyhetsbrevet kan gjøre at '. $user->getName() .' ikke får velkomst-epost. '.
-                'Vi jobber med å rette feilen'
+                'En feil med nyhetsbrevet kan gjøre at ' . $user->getName() . ' ikke får velkomst-epost. ' .
+                'Vi jobber med å rette feilen. Systemet sa: '. $e->getMessage()
             );
         }
 
         UKMnettverket::getFlash()->add(
             'success',
-            ($created ? 
-                'Bruker er oppprettet for '. $user->getName() .' og '
+            ($created ?
+                'Bruker er oppprettet for ' . $user->getName() . ' og '
                 : $user->getName() . ' er ') .
-            'lagt til som administrator for ' . $omrade->getNavn()
+                'lagt til som administrator for ' . $omrade->getNavn()
         );
     }
 }
