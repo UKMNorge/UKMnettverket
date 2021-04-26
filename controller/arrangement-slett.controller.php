fe6dab5ce39c2dfda86f0ef243614ecbccac2016
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2021-04-26 19:28:12 +0200

    Sletting av arrangement sender påmeldte bare til same type
    Close #81
    
    Utviklingstid: 3 timer

--- controller/arrangement-slett.controller.php
+++ controller/arrangement-slett.controller.php
@@ -7,6 +7,17 @@ use UKMNorge\Wordpress\Blog;
 $arrangement = new Arrangement( $_GET['arr'] );
 $omrade = new Omrade($_GET['type'], (Int)$_GET['omrade']);
 
+
+$arrangementerSammeType = [];
+foreach($omrade->getKommendeArrangementer()->getAll() as $arrang) {
+    if($arrang->getId() != $arrangement->getId() && $arrang->erArrangement() == $arrangement->erArrangement()) {
+        $arrangementerSammeType[] = $arrang;
+    }
+}
+
+
+
+UKMnettverket::addViewData('arrangementerSammeType', $arrangementerSammeType);
 UKMnettverket::addViewData('arrangement', $arrangement);
 UKMnettverket::addViewData('omrade', $omrade);
 UKMnettverket::addViewData('sesong', get_site_option('season'));
